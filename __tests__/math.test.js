92b9cc71c19959a4be031b63fd57d487f0ae53c0
James Buczkowski <james.sullenriot@gmail.com>
2017-06-16 21:31:59 -0400

    Changed test names and added more math tests

---
+++ __tests__/math.test.js
@@ -0,0 +1,25 @@
+const {
+  addition,
+  subtraction,
+  multiply,
+  divide
+} = require("../src/math.js");
+
+
+describe('example', function() {
+  it('finds the sum of two values', function() {
+    expect(addition(1, 2)).toBe(3);
+  });
+
+  it('subtracts two values', function() {
+    expect(subtraction(5, 3)).toBe(2);
+  });
+
+  it('multiplies two values', function() {
+    expect(multiply(5, 5)).toBe(25);
+  });
+
+  it('divides two values', function() {
+    expect(divide(10, 2)).toBe(5);
+  });
+});
